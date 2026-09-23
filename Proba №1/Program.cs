5287fb48b4e631f77407c8cbeb4677a32860ddf3
Lanay072 <wlados1243@gmail.com>
2026-09-24 00:43:17 +0300

    add logic to age

--- Proba №1/Program.cs
+++ Proba №1/Program.cs
@@ -1,7 +1,28 @@
 ﻿Console.WriteLine("твое имя");
 string name = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(name))
+{
+    Console.WriteLine("блять какой ты тупой");
+}
+else
+{ Console.WriteLine("ебать ты лев");
+}
+
 Console.WriteLine("сколько лет");
 int age = int.Parse(Console.ReadLine());
+if (age<0 || age>120)
+{ Console.WriteLine("пиздабол!"); }
+else if (age <= 12)
+{
+    Console.WriteLine("ты пиздюк");
+}
+else if (age >= 13 && age < 18)
+{
+    Console.WriteLine("подросток");
+}
+else
+        { Console.WriteLine("ебать ты дядя"); }
+
 Console.WriteLine("в каком городе живёшь");
 string city = Console.ReadLine();
 
