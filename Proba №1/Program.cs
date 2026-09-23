ed99511accaf46de6e8bb75da89b0b9eeafea77c
Lanay072 <wlados1243@gmail.com>
2026-09-23 23:58:53 +0300

    compleat №1 "displaying user information"

--- Proba №1/Program.cs
+++ Proba №1/Program.cs
@@ -1,3 +1,13 @@
-﻿Console.WriteLine("Продам сел-поехал, без второй передачи!");
-Console.WriteLine("Третья вылетает ;(");
-Console.WriteLine("Сел поехал разъебался((");
+﻿Console.WriteLine("моё имя");
+string govno = Console.ReadLine();
+Console.WriteLine("столько лет");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine("одинок да/нет");
+string pizda = Console.ReadLine();
+
+
+Console.WriteLine("===== Информация о пользователе =====");
+Console.WriteLine($"Тебя еблана зовут = {govno}");
+Console.WriteLine($"тебе ебанату стока лет = {age}");
+Console.WriteLine($"ты хуесос просто {pizda}");
+Console.WriteLine("=====================================");
